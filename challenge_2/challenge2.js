836ede1704e2d813fa1280b84c1b67096f76843f
Monte Williams <monte.williams@gmail.com>
2017-02-04 10:52:09 -0500

    Added heading color toggle and hid odd sections

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,22 @@
 $(function(){
+  var title = $("h1");
+  var currentColor = title.css("color");
 
+
+  title.click(function() {
+    if ($(this).css("color") === currentColor) {
+      $(this).css("color", "red");
+    }
+    else {
+      $(this).css("color", currentColor);
+    }
+  })
+
+
+  $(".title").filter(":even").hide();
+
+
+  
 	//all your code should go here
 
 })
